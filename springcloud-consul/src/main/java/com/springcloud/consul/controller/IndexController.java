4b9fb12f4cc8daade034c6773f95946ff4af5209
xiaolanchong <1941456312@qq.com>
2020-04-19 19:31:35 +0800

    consul注册中心

---
+++ springcloud-consul/src/main/java/com/springcloud/consul/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.springcloud.consul.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping
+public class IndexController {
+
+    @GetMapping("index")
+    public Object index(){
+        return "Hello Word";
+    }
+
+}
